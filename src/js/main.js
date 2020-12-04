f1ecb94b90b027421f8621d16d9a973752c06ede
Rafael Batista <maranata_rafa@hotmail.com>
2020-12-04 19:55:18 -0400

    added currency filter in price of products

--- src/js/main.js
+++ src/js/main.js
@@ -3,6 +3,11 @@ const vm = new Vue({
   data: {
     produtos: []
   },
+  filters: {
+    numeroPreco(valor) {
+      return valor.toLocaleString("pt-BR", { style: "currency", currency: "BRL" });
+    }
+  },
   methods: {
     async obterProdutos() {
       let promise = await fetch("./api/produtos.json");
