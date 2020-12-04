8f6e169c73eff0d4911381d8e52864516b7cebc1
Rafael Batista <maranata_rafa@hotmail.com>
2020-12-04 10:00:10 -0400

    added fetch products

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,15 @@
 const vm = new Vue({
   el: "#app",
   data: {
-    welcome: "Olá mundo"
+    produtos: []
+  },
+  methods: {
+    async obterProdutos() {
+      let promise = await fetch("./api/produtos.json");
+      this.produtos = await promise.json();
+    }
+  },
+  created() {
+    this.obterProdutos();
   }
 });
